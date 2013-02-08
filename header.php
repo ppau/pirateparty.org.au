95918c534aa9bc6a173db903190e7bb3e5d9ade4
Thomas Randle <thomas@tasgn.com>
2013-02-08 21:29:12 +1100

    remove php datetime countdown setter

--- header.php
+++ header.php
@@ -37,18 +37,6 @@
 	
 	<?php wp_head(); ?>
 
-	<script type="text/javascript" charset="utf-8">
-	var dst_offset = "<?php 
-	$tz = new DateTimeZone("Australia/Sydney");
-	if (count($tz->getTransitions(time())) > 0)
-	{
-		echo 11;
-	}else{
-		echo 10;
-	}
-	?>:00";
-	</script>
-
 </head>
 
 <body <?php body_class(); ?>>
