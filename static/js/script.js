33e2911b30ec93e30f7e00d55cb9b95696c681c8
Thomas Randle <thomas@tasgn.com>
2016-06-30 14:38:15 +1000

    fixes iOS menu issues

--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,12 @@
 /* Author: Thomas Randle
- Date: 2012
+ Date: 2016
  */
 
-$(function() {
-
-	/*
-	$("#amount").change(function(e) {
-		if ($("#amount").val() == "other") {
-			$("#other_amount").show();
-		} else {
-			$("#other_amount").hide();
-		}
-	}).trigger("change");
-
-	$('.modal').on('hidden', function() {
-		$('.modal-backdrop').remove();
-		$(this).find('.youtube').each(function() {
-			if ( typeof this.stopVideo == 'function') {
-				this.stopVideo();
-			}
-		});
-	})
-	*/
+$('.dropdown-toggle').click(function(e) {
+  e.preventDefault();
+  setTimeout($.proxy(function() {
+    if ('ontouchstart' in document.documentElement) {
+      $(this).siblings('.dropdown-backdrop').off().remove();
+    }
+  }, this), 0);
 });
